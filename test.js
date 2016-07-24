02ec3d9297f80cdae83db7345980ab08fdeef619
peecky <crimsonpi@gmail.com>
2016-07-24 22:51:05 +0900

    Update test to list all available functions

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 
 let func = require('./').getKRMovies;
 //func = require('./').getCGVMovies;
+//func = require('./').getLottecinemaMovies;
+//func = require('./').getMegaboxMovies;
 func({ genre: '애니메이션' }, (err, items) => {
     if (err) throw err;
     console.log(items);
