ed7f8c50a019d0a01188d3e1d8ff61fa07a60cd6
peecky <crimsonpi@gmail.com>
2016-07-24 22:50:34 +0900

    Change function name

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let func = require('./').getKoMovies;
+let func = require('./').getKRMovies;
 //func = require('./').getCGVMovies;
 func({ genre: '애니메이션' }, (err, items) => {
     if (err) throw err;
